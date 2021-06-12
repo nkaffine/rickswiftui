592234a7552172842234e434e8183a98a835b528
Nicholas Kaffine <nicholas.kaffine@gmail.com>
2021-06-12 10:19:18 -0400

    Initial Commit

---
+++ RickSwiftUI/RickSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickSwiftUIApp.swift
+//  RickSwiftUI
+//
+//  Created by Nicholas Kaffine on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RickSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
